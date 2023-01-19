c860050e630fd9bed9e506d015959b1e6334fdf6
Vurhis1 <denraimond@gmail.ru>
2023-01-19 19:40:53 +0300

    add code

---
+++ 7.3/playwright/tests/Netology.test.js
@@ -0,0 +1,31 @@
+const { test, expect } = require("@playwright/test");
+import { userEmail, userPass } from "../user";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+});
+
+test("Success authorization netology", async ({ page }) => {
+  await page.locator('[placeholder = "Email"]').fill(userEmail);
+  await page.locator('[placeholder = "Пароль"]').fill(userPass);
+  await page.locator('[data-testid="login-submit-btn"]').click();
+  const header = await page.locator("h2");
+  await expect(header).toHaveText("Мои курсы и профессии");
+
+  await page.screenshot({
+    path: "image/screenshot_" + Date.now() + ".png",
+  });
+});
+
+test("Failed authorization netology", async ({ page }) => {
+  await page.locator('[placeholder = "Email"]').fill("exemple@gmail.com");
+  await page.locator('[placeholder = "Пароль"]').fill(userPass);
+  await page.locator('[data-testid = "login-submit-btn"]').click();
+
+  const errorText = page.locator('[data-testid="login-error-hint"]');
+  await expect(errorText).toHaveText("Вы ввели неправильно логин или пароль");
+
+  await page.screenshot({
+    path: "image/screenshot_" + Date.now() + ".png",
+  });
+});
