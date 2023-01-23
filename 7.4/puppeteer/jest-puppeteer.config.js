3ddab00ebe961320d36652c66a9b15aef8bfeaa8
Vurhis1 <denraimond@gmail.ru>
2023-01-23 15:17:56 +0300

    change gh.test.js

--- 7.4/puppeteer/jest-puppeteer.config.js
+++ 7.4/puppeteer/jest-puppeteer.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    launch: {
-        headless: false,
-        defaultViewport: null,
-        args: ['--start-maximized'] //— используем максимальный размер окна браузера
-      },
-    
-  };
-  
+  launch: {
+    slowmo: 5000,
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"], //— используем максимальный размер окна браузера
+  },
+};
