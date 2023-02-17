322952a2338a5763bd2eedf5cdd1a25a216b8c43
Vurhis1 <denraimond@gmail.ru>
2023-02-17 11:35:04 +0300

    1

--- CypressHM2(SQL)/cypress.config.js
+++ CypressHM2(SQL)/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "dkgma3",
   e2e: {
     baseUrl: "https://petstore.swagger.io/v2/pet/",
     setupNodeEvents(on, config) {
